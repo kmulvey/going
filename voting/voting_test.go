66d2eabecba933c113d022df492e84181d894eda
Kevin Mulvey <kmulvey@linux.com>
2015-02-15 10:46:18 -0500

    test names

--- voting/voting_test.go
+++ voting/voting_test.go
@@ -16,17 +16,17 @@ func TestVoteTwo(t *testing.T) {
 	votes := [4]string{"Teru", "Teru", "Jun", "Jun"}
 	assert.Equal(t, "", tally(names[:], votes[:]), "they should be equal")
 }
-func TestCalcThree(t *testing.T) {
+func TestVoteThree(t *testing.T) {
 	names := [4]string{"Toshi", "Jun", "Teru", "Chihiro"}
 	votes := [4]string{"Toshi", "Toshi", "Jun", "Jun"}
 	assert.Equal(t, "Jun", tally(names[:], votes[:]), "they should be equal")
 }
-func TestCalcFour(t *testing.T) {
+func TestVoteFour(t *testing.T) {
 	names := [2]string{"Toshi", "Jun"}
 	votes := [2]string{"Toshi", "Jun"}
 	assert.Equal(t, "", tally(names[:], votes[:]), "they should be equal")
 }
-func TestCalcFive(t *testing.T) {
+func TestVoteFive(t *testing.T) {
 	names := [4]string{"PhpLove", "phplove", "phpLove", "Phplove"}
 	votes := [4]string{"phpLove", "phpLove", "phpLove", "PhpLove"}
 	assert.Equal(t, "phpLove", tally(names[:], votes[:]), "they should be equal")
