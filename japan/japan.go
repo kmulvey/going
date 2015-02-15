e35ce25591bbc2c07be78172f60a8105f416dc7e
Kevin Mulvey <kmulvey@linux.com>
2015-02-15 09:54:50 -0500

    move and use large array

--- japan/japan.go
+++ japan/japan.go
@@ -3,7 +3,7 @@ package japan
 import "fmt"
 
 func main() {
-	places := [5]int{30, 20, 20, 10}
+	places := [20]int{712, 745, 230, 200, 648, 440, 115, 913, 627, 621, 186, 222, 741, 954, 581, 193, 266, 320, 798, 745}
 	fmt.Println("Total: ", calc(places[:]))
 }
 func calc(places []int) int {
